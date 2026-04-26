05f52a55d010e173cf030dbcea4c2e8d36b4b973
kevin nayar <kevin.nayar@tryalclinical.com>
2026-04-25 22:53:31 -0500

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,22 +5,19 @@ export function Footer() {
         "A dram is a thing best measured not in ounces, but in minutes earned."
       </div>
       <div>
-        <h4>Sections</h4>
+        <h4>Whiskies by type</h4>
         <ul>
-          <li><a href="/whiskies?type=Scotch">Scotch</a></li>
+          <li><a href="/whiskies">All</a></li>
           <li><a href="/whiskies?type=Bourbon">Bourbon</a></li>
-          <li><a href="/whiskies?type=Rye">Rye</a></li>
           <li><a href="/whiskies?type=Irish">Irish</a></li>
-          <li><a href="/whiskies?type=Whisky">Whisky</a></li>
         </ul>
       </div>
       <div>
-        <h4>Archive</h4>
+        <h4>&nbsp;</h4>
         <ul>
-          <li>Vol. XII · Spring 2026</li>
-          <li>Editorial notes</li>
-          <li>Scoring rubric</li>
-          <li>Submission policy</li>
+          <li><a href="/whiskies?type=Rye">Rye</a></li>
+          <li><a href="/whiskies?type=Scotch">Scotch</a></li>
+          <li><a href="/whiskies?type=Whisky">Whisky</a></li>
         </ul>
       </div>
     </footer>
